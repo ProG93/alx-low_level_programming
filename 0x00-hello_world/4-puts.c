426ceb3a25ddb28402bde1352cc886dfc1df0f4b
Progress <progress.goga@gmail.com>
2023-02-16 08:09:58 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
-int main (void)
+/**
+ * Low level programming
+ * prints string using puts
+ */
+
+int main(void)
 {
-puts("Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle");
 return 0;
-}
