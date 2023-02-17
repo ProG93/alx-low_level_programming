9134caaafa2545f10e42023130ca6157f4beea81
Progress <progress.goga@gmail.com>
2023-02-17 10:17:44 -0800

    alpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - putschar
+ * Return: 0(Success)
+ */
+
+int main(void)
+{
+	char x;
+
+	for (char x = 'A'; x <= 'Z'; x++)
+	{
+		char = tolower(x);
+		putchar(x);
+		putchar('\n');
+	}
+	return (0);
+}
