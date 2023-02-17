d4f0abcc4e7962e26cef882630268118f8357db0
Progress <progress.goga@gmail.com>
2023-02-17 12:32:40 -0800

    numbrz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (n > 10)
+	{
+		putchar(48 + n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
