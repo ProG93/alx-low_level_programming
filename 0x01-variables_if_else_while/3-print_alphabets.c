a6eeaa90c16f223c9a9ad9745d314f5649284619
Progress <progress.goga@gmail.com>
2023-02-17 11:46:37 -0800

    alpha

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,12 +7,15 @@
 int main(void)
 {
 	char alphabet;
-	char upper = toupper(alphabet);
 
 	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 	{
 		putchar(alphabet);
-		putchar(upper);
+	}
+
+	for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
+	{
+		putchar(alphabet);
 	}
 	putchar('\n');
 	return (0);
