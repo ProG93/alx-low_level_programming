ac63f0e1e1327d59de0999c8d41aff656539f1a3
Progress <progress.goga@gmail.com>
2023-02-17 13:00:56 -0800

    base

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - variables
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putcchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
