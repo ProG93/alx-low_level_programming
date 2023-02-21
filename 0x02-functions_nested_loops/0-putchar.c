052dcd3d8d59e8f5932869bdc49a54e092f98507
Progress <progress.goga@gmail.com>
2023-02-21 09:51:16 -0800

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,23 +1,20 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * main - Entry point
- * Description - putchar
- * Return 0(Success)
+ * main - Prints _putchar as a message
+ *
+ * Return: 0(Success)
  */
+
 int main(void)
-
 {
-	_putchar("_");
-	_putchar("p");
-	_putchar("u");
-	_putchar("t");
-	_putchar("c");
-	_putchar("h");
-	_putchar("a");
-	_putchar("r");
-	_putchar('\n');
-
-	return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
