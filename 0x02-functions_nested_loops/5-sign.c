1e53c34f0de7365c10f1b579179852c7fc2ee356
Progress <progress.goga@gmail.com>
2023-02-21 12:34:15 -0800

    task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_sign - determines the sign
+ * @n: is a number
+ * Return: 1 if number is > 0, 0 if number = 0 otherwise -1
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+}
