5f63cfddd2aab474be19daa53844be7f5fb48000
Progress <progress.goga@gmail.com>
2023-02-21 13:54:35 -0800

    task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_last_digit - print last digit
+ * @n: is the number
+ * Return: last_digit
+ */
+int print_last_digit(int n)
+{
+	int a;
+
+	a = n % 10;
+
+	_putchar(a);
+	return (0);
+}
+
+
