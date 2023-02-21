95edb635dea5a568491e591cbe125cc449fb72de
Progress <progress.goga@gmail.com>
2023-02-21 03:48:46 -0800

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <main.h>
+
+/**
+ * main - functions and nested loops
+ * Return: 0(Success)
+ */
+int main(void)
+{
+	char p = "_putchar";
+
+	while (p)
+	{
+		_putchar(p);
+		p++;
+	}
+	_putchar('\n');
+	return (0);
+}
