67757d6c34cd761ed07800039a2a8026587b065b
Progress <progress.goga@gmail.com>
2023-02-22 00:21:06 -0800

    task 8

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,10 +9,10 @@ void print_to_98(int n)
 	if (n <= 98)
 	{
 		for (n = n; n <= 97; n++)
-			_putchar("%d, ", n);
+			_putchar(char *n);
 	}
 	else
 		for (n = n; n > 98; n--)
-			_putchar("%d, ", n);
+			_putchar(char *n);
 	_putchar("98\n");
 }
