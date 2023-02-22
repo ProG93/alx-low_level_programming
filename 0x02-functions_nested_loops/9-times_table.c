7da4e87bfea8a1defbc061973aad7d7beaaad778
Progress <progress.goga@gmail.com>
2023-02-22 02:31:13 -0800

    times

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,22 +4,26 @@
  */
 void times_table(void)
 {
-	int i, j, k;
+	int num, mult, prod;
 
-
-	for (j = 0; j <= 9; j++)
+	for (num = 0; num <= 9; num++)
 	{
-		_putchar(',');
-		_putchar(' ');
+		_putchar('0');
 
-		k = j * i;
-
-		if (k <= 9)
+		for (mult = 1; mult <= 9; mult++)
+		{
+			_putchar(',');
 			_putchar(' ');
-		else
-			_putchar((k / 10) + '0');
-		_putchar((k % 10) + '0');
-	}
-	_putchar('\n');
 
+			prod = num * mult;
+
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_putchar((prod / 10) + '0');
+
+			_putchar((prod % 10) + '0');
+		}
+		_putchar('\n');
+	}
 }
