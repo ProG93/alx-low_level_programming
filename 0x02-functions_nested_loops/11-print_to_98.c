409f04494f69ac9c3170ef8d2d2891575ae26486
Progress <progress.goga@gmail.com>
2023-02-22 00:12:15 -0800

    task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_to_98 - prints all natural numbers fron n to 98
+ * @n: starting point
+ * Return: returns nothing
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (n = n; n <= 97; n++)
+			_putchar("%d, ", n);
+	}
+	else
+		for (n = n; n > 98; n--)
+			_putchar("%d, ", n);
+	_putchar("98\n");
+}
