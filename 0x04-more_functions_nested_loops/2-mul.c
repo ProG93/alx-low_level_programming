8f0e62894eb06d2ea2794c11f232fa0802f919c1
Progress <progress.goga@gmail.com>
2023-02-23 03:53:53 -0800

    multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - multiply intergers
+ * @a: first integer
+ * @b: second integer
+ * Return: 0(always)
+ */
+int mul(int a, int b)
+{
+	int x;
+
+	x = a * b;
+		_putchar(x);
+
+	return (0);
+}
