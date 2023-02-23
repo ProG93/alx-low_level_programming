1b3e5b6154a8bd47224bd929d64fc903657ec24b
Progress <progress.goga@gmail.com>
2023-02-23 13:19:14 -0800

    fizz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,19 +11,19 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 15 == 0)
-			printf("FizzBuzz\n");
+			_putchar("FizzBuzz");
 
 		else if ((i % 3) == 0)
-			printf("Fizz\n");
+			_putchar("Fizz");
 
 		else if ((i % 5) == 0)
-			printf("Buzz\n");
+			_putchar("Buzz");
 
 		else
-			printf("%d\n", i);
+			_putchar(i);
 
 	}
-
+	_putchar('\n');
 	return (0);
 }
 
