aed99be84f941664a297d9b2446bf531cc3a9d83
Progress <progress.goga@gmail.com>
2023-02-28 23:35:27 -0800

    main

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strncat - print out two strings
+ * @dest: - destination of pointer
+ * @src: source of pointer
+ * @n: at most bytes
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+
