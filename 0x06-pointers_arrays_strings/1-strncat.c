2ced3c4786c276122d585cb174ac64b33aa20882
Progress <progress.goga@gmail.com>
2023-03-01 11:31:08 -0800

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,4 +10,22 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
+	int b;
+	int i;
+
+	b = 0;
+	while (dest[b] != '\0')
+	{
+		b++;
+	}
+	i = 0;
+	while (i < n && src[i] != '\0')
+	{
+		dest[b] = src[i];
+		b++;
+		i++;
+	}
+	dest[b] = '\0';
+	return (dest);
+}
 
