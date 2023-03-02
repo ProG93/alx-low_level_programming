51e7561acdd971c024415083a262239c1ba09c4b
Progress <progress.goga@gmail.com>
2023-03-02 01:53:33 -0800

    upper string

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * string_toupper - change lowercase letters to uppercase.
+ * @p: analized string.
+ *
+ * Return: String 'p'.
+ */
+char *string_toupper(char *p)
+{
+	int i = 0;
+
+	while (p[i] != '\0')
+	{
+		if (*p[i] >= 'a' && p[i] <= 'z')
+		{
+			p[i] = p[i] - 32;
+		}
+		i++;
+	}
+	return (p);
+}
