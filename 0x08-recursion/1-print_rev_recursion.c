b55a8e5097f4fec8f17469a8848ea37a5124081b
Progress <progress.goga@gmail.com>
2023-03-08 06:32:30 -0800

    Reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _print_rev_recursion - print reversed string
+* @s: string
+* Return: nothing
+*/
+
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_print_rev_recursion((s + 1));
+	}
+}
