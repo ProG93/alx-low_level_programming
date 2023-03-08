19feba3f153b978786f81f33d28b5989769dab01
Progress <progress.goga@gmail.com>
2023-03-08 08:51:12 -0800

    sqt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,9 +9,9 @@ int sqrt_function(int n, int i);
 */
 int _sqrt_recursion(int n)
 {
-        if (n < 0)
-                return (-1);
-        return (sqrt_function(n, n + 1));
+	if (n < 0)
+		return (-1);
+	return (sqrt_function(n, 0));
 }
 
 /**
@@ -24,9 +24,9 @@ int _sqrt_recursion(int n)
  */
 int sqrt_function(int n, int i)
 {
-        if (i * i > n)
-                return (1);
-        if (i * i == n)
-                return (i);
-        return (sqrt_function(n, i + 1));
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (sqrt_function(n, i + 1));
 }
