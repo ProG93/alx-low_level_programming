32aeaf70e694b35f3cb5a7ed7b750ee1a52af94e
Progress <progress.goga@gmail.com>
2023-03-08 07:53:30 -0800

    sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,11 +10,24 @@
 
 int _sqrt_recursion(int n)
 {
-	int k;
-
 	if (n < 0)
 		return (-1);
-	if (n == 0)
-		return (1);
-	return (_sqrt_recursion(n, k + 1));
+	return (sqrt_function(n, 0));
+}
+
+/**
+ * sqrt_function - recurses to find the natural
+ * square root of a number
+ * @n: number to calculate the sqaure root of
+ * @i: iterator
+ *
+ * Return: the resulting square root
+ */
+int sqrt_function(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (sqrt_function(n, i + 1));
 }
