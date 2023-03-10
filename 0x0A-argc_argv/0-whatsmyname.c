b6e5d5fb5c222b0755b9a4980fd0e568964f5315
Progress <progress.goga@gmail.com>
2023-03-10 10:05:11 -0800

    num

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include "main.h"
-
 /**
- * main - prints name
- * @argc: number of arguments past
- * @argv: points to argument in array
- * Return: 0
+ * main - Print the name of the program
+ * @argc: Count arguments
+ * @argv: Arguments
+ *
+ * Return: Always 0 (Success)
  */
+
 int main(int argc, char *argv[])
 {
-	(void)argc;
+	(void) argc;
 	printf("%s\n", argv[0]);
 	return (0);
 }
