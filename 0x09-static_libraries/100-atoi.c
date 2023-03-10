5574e9f49084f90c5911b43427bdb184ece8cebf
Progress <progress.goga@gmail.com>
2023-03-10 06:36:06 -0800

    atoi

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -3,8 +3,16 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
+/**
+* _atoi - change string to interger
+* @s: string
+* Return: interger
+*/
 int _atoi(char *s)
-
 {
-	return atoi(s);
+	int res = 0;
+
+	for (int i = 0; s[i] != '\0'; i++)
+	res = res * 10 + s[i] - '0';
+	return (res);
 }
