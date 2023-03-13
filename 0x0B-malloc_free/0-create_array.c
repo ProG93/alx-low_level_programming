63730aba3c328c9293ed71a46e1bb21070e81218
Progress <progress.goga@gmail.com>
2023-03-13 06:37:22 -0700

    create array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * create_array - creates an array of chars
+ * @size: size of the memory to the string
+ * @c: char to be created
+ * Return: NULL
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *array;
+
+	if (size == 0)
+		return (NULL);
+	array = (char *) malloc(size * sizeof(char));
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		array[i] = c;
+	}
+	return (array);
+}
