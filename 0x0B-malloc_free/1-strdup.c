d970fcff5def0bf46a16b7f5ee161cc51483d1cd
Progress <progress.goga@gmail.com>
2023-03-13 08:34:38 -0700

    strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - returns a pointer to be duplicated string
+ * @str: string
+ * Return: NULL
+ */
+
+char *_strdup(char *str)
+{
+	char *d;
+	unsigned int i, k;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; str[i] != '\0'; i++)
+	if (i < 1)
+		return (NULL);
+	d = malloc(sizeof(char) * i);
+	if (d == NULL)
+	{
+		free(d);
+		return (NULL);
+	}
+	for (k = 0; k < i; k++)
+		d[k] = str[k];
+	d[k] = '\0';
+
+	return (d);
+}
