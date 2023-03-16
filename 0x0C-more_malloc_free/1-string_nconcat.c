ea110ba417ad7cd4cedf21a524fa65f8b5b59b4f
Progress <progress.goga@gmail.com>
2023-03-16 04:44:07 -0700

    string

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,27 +13,29 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *cat;
-	unsigned int i, size1, size2;
+	unsigned int i = 0, size1 = 0, size2 = 0, j = 0;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	for (size1 = 0; s1[size1] != '\0'; size1++)
-	for (size2 = 0; s2[size2] != '\0'; size2++)
-	if (n > size2)
-		n = size2;
-	cat = malloc((size1 + n + 1) * sizeof(char));
-	if (cat == NULL)
-		return (0);
-	for (i = 0; i < size1; i++)
+	while (s1 && s1[size1])
+		size1++;
+	while (s2 && s2[size2])
+		size2++;
+	if (n < size2)
+		cat = malloc(sizeof(char) * (size1 + n + 1);
+	else
+		cat = malloc(sizeof(char) * (size1 + size2 + 1);
+	if (!cat)
+		return (NULL);
+	while (i < size1)
 	{
 		cat[i] = s1[i];
+		i++;
 	}
-	for (; i < (size1 + n); i++)
-	{
-		cat[i] = s2[i - size1];
-	}
+
+	while (n < size2 && i < (size1 + n))
+		cat[i++] = s2[j++];
+	while (n >= size2 && i < (size1 + size2))
+		cat[i++] = s2[j++];
 	cat[i] = '\0';
+
 	return (cat);
 }
