75df1030e1f632c81a4a1575b3c626971c69bd07
Progress <progress.goga@gmail.com>
2023-03-22 08:38:48 -0700

    2 index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,28 +11,16 @@
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i, res;
+	int i;
 
-	res = -1;
-	if (array && cmp)
+	if (array == NULL || size <= 0 || cmp == NULL)
+		return (-1);
+
+
+	for (i = 0; i < size; i++)
 	{
-		if (size <= 0)
-		{
-			return (res);
-		}
-		for (i = 0; i < size; i++)
-		{
-			cmp(array[i]);
-			if (cmp(array[i]) > 0)
-			{
-				res = i;
-				break;
-			}
-			if ((cmp(array[i]) == (-1)))
-			{
-				return (res);
-			}
-		}
+		if (cmp(array[i]))
+			return (i);
 	}
-	return (res);
+	return (-1);
 }
