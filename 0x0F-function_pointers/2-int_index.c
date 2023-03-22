ee20df0f06ea2d8016277f5047fdce4b78210f5c
Progress <progress.goga@gmail.com>
2023-03-22 08:20:31 -0700

    index

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+#include <stdio.h>i
+
+/**
+ * int_index - function that searches interger
+ * @array: array of integers
+ * @size: number of elements in an array
+ * @cmp: pointer to function
+ * Return: 0 return -1
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || size <= 0 || cmp == NULL)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+	}
+	return (i);
+}
