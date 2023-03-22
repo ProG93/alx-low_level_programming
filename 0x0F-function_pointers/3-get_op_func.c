3e5d2e4304e8491a0497027bbb671f3f6e272aaf
Progress <progress.goga@gmail.com>
2023-03-22 09:00:27 -0700

    op function

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,12 +1,9 @@
 #include "3-calc.h"
-#include <stdlib.h>
 /**
- * get_op_func - Selects the correct function to perform
- * the operation asked by the user.
- * @s: The operator passed as argument.
+ *get_op_func - function selects the correct function to perform.
+ *@s: operator used.
  *
- * Return: A pointer to the function corresponding
- * to the operator given as a parameter.
+ *Return: correct function result or NULL if operator is wrong.
  */
 int (*get_op_func(char *s))(int, int)
 {
@@ -16,13 +13,20 @@ int (*get_op_func(char *s))(int, int)
 		{"*", op_mul},
 		{"/", op_div},
 		{"%", op_mod},
-		{NULL, NULL},
-	};
+		{NULL, NULL}
+		};
+	
+	int i;
 
-	int i = 0;
-
-	while (ops[i].op != NULL && *(ops[i].op) != *s)
+	i = 0;
+	while (i < 5 && s != NULL)
+	{
+		if (s[0] == *(ops[i].op))
+		{
+			return (ops[i].f);
+		}
 		i++;
+	}
 
-	return (ops[i].f);
+	return (NULL);
 }
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,28 +1,71 @@
 #include "3-calc.h"
-#include <stdlib.h>
 /**
- * get_op_func - Selects the correct function to perform
- * the operation asked by the user.
- * @s: The operator passed as argument.
+ *op_add - return the sum of 2 numbers.
+ *@a: integer.
+ *@b: integer.
  *
- * Return: A pointer to the function corresponding
- * to the operator given as a parameter.
+ *Return: sum.
  */
-int (*get_op_func(char *s))(int, int)
+int op_add(int a, int b)
 {
-	op_t ops[] = {
-		{"+", op_add},
-		{"-", op_sub},
-		{"*", op_mul},
-		{"/", op_div},
-		{"%", op_mod},
-		{NULL, NULL},
-	};
+	return (a + b);
+}
 
-	int i = 0;
 
-	while (ops[i].op != NULL && *(ops[i].op) != *s)
-		i++;
+/**
+ *op_sub - returns difference of 2 numbers
+ *@a:  integer.
+ *@b: integer.
+ *
+ *Return: difference.
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
 
-	return (ops[i].f);
+/**
+ *op_mul - returns product of two numbers.
+ *@a: integer.
+ *@b: integer.
+ *
+ *Return: product.
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ *op_div - returns the quotient of 2 numbers.
+ *@a: integer.
+ *@b:integer.
+ *
+ *Return: quotient.
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
+}
+
+/**
+ *op_mod - returns the remainder of division.
+ *@a: integer.
+ *@b: integer.
+ *
+ *Return: remainder.
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
 }
