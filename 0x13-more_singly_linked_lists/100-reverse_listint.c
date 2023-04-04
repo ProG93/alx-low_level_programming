7e6dd2b09971757de9e1cd3a0be416badc810227
Progress <progress.goga@gmail.com>
2023-04-04 06:34:31 -0700

    changes

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -2,28 +2,24 @@
 
 /**
  * reverse_listint - reverses a linked list
- * @head: double pointer to the start of the list
- * Return: a pointer to the first node of the reversed list
+ * @head: pointer to the first node in the list
+ *
+ * Return: pointer to the first node in the new list
  */
 listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *prev, *next;
+	listint_t *prev = NULL;
+	listint_t *next = NULL;
 
-	if (head == NULL || *head == NULL)
-		return (NULL);
-
-	if ((*head)->next == NULL)
-		return (*head);
-
-	prev = NULL;
-
-	while (*head != NULL)
+	while (*head)
 	{
 		next = (*head)->next;
 		(*head)->next = prev;
 		prev = *head;
 		*head = next;
 	}
+
 	*head = prev;
+
 	return (*head);
 }
--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -23,7 +23,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		new->n = n;
 		*head = new;
 
-		 return (new);
+		return (new);
 	}
 	while (tmp->next != NULL)
 	{
