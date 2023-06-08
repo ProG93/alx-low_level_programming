eb54a7a4a913d61a0a090c496582ced004ae6074
Progress <progress.goga@gmail.com>
2023-06-08 01:48:34 -0700

    hash tables

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,23 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieves a value
+ * @ht: hash table
+ * @key: key to find
+ * Return: value associated element
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int i, index;
+
+	if (!ht || *key == '\0')
+		return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	for (i = index; ht->arrat[i]; i++)
+	{
+		if (strcmp(ht->array[i]->key, key) == 0)
+			return (ht->array[i]->value);
+	}
+	return (NULL);
+}
