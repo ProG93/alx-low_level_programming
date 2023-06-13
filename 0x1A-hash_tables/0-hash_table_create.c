a4288271b76c4c6523e676a6c6f52b15b148b315
Progress <progress.goga@gmail.com>
2023-06-13 03:07:40 -0700

    task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -19,7 +19,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	new_table->array = malloc(sizeof(hash_node_t *) * size);
 
 	if (new_table->array == NULL)
-		return(NULL);
+		return (NULL);
 
 	return (new_table);
 }
